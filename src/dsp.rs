b445afee6d75c4423157a8eca39abaf41a4f3579
Nico Chatzi <nico.chatzigianis@focusrite.com>
2021-02-18 14:05:35 +0000

    Add last hack's dsp

--- src/dsp.rs
+++ src/dsp.rs
@@ -1,79 +1,268 @@
-use alloc::boxed::Box;
+#[macro_use]
 use rume::*;
 
-mod table {
-    pub const SIZE: usize = 256;
-    pub const FREQ: f32 = 48_000. / SIZE as f32;
-    pub const TIME: f32 = 1. / FREQ;
+#[derive(Debug, Clone)]
+pub struct Delay {
+    pub input: (DelaySampleInput, DelayTimeInput),
+    pub output: DelaySampleOutput,
+    sample: f32,
+    delay_ticks: f32,
+    sample_rate: u32,
+    memory: [f32; 44_100],
+    read_idx: f32,
+    write_idx: usize,
+    buffer_size: usize,
+}
+
+impl Default for Delay {
+    fn default() -> Delay {
+        Delay {
+            input: (DelaySampleInput, DelayTimeInput),
+            output: DelaySampleOutput,
+            sample: 0.0,
+            delay_ticks: 0.0,
+            sample_rate: 44_100,
+            memory: [0.0; 44_100],
+            read_idx: 0.0,
+            write_idx: 0,
+            buffer_size: 0,
+        }
+    }
+}
+
+input! { Delay, DelayTimeInput,
+    |proc: &mut Delay, time_ms: f32| {
+        proc.delay_ticks = (time_ms * 0.001) * proc.sample_rate as f32;
+        proc.buffer_size = proc.memory.len();
+    }
+}
+
+input! { Delay, DelaySampleInput,
+    |proc: &mut Delay, sample: f32| {
+        proc.sample = sample;
+    }
+}
+
+output! { Delay, DelaySampleOutput,
+    |proc: &mut Delay| -> f32 {
+        proc.sample
+    }
+}
+
+#[inline(always)]
+fn lerp(a: f32, b: f32, w: f32) -> f32 {
+    a + w * (b - a)
+}
+
+impl Processor for Delay {
+    fn prepare(&mut self, data: AudioConfig) {
+        self.sample_rate = data.sample_rate as u32;
+    }
+
+    fn process(&mut self) {
+        let buffer_size = self.memory.len();
+
+        self.memory[self.write_idx] = self.sample;
+        self.write_idx = (self.write_idx + 1) % buffer_size;
+        self.read_idx = (self.write_idx as f32 - self.delay_ticks) % buffer_size as f32;
+
+        let read_idx_0 = self.read_idx as usize;
+        let read_idx_1 = (read_idx_0 + 1) % buffer_size;
+
+        let wet = lerp(
+            self.memory[read_idx_0],
+            self.memory[read_idx_1],
+            self.read_idx % 1.0,
+        );
+
+        self.sample = lerp(self.sample, wet, 0.5);
+    }
 }
 
 #[rume::processor]
-pub struct Oscillator {
+pub struct Distortion {
+    #[sample]
+    sample: f32,
+
+    #[input]
+    amount: f32,
+}
+
+impl Processor for Distortion {
+    fn prepare(&mut self, _: AudioConfig) {}
+
+    #[inline(always)]
+    fn process(&mut self) {
+        self.sample = (self.amount * self.sample).tanh();
+    }
+}
+
+#[rume::processor]
+pub struct Sine {
     #[input]
     frequency: f32,
 
     #[input]
-    fm: f32,
+    amplitude: f32,
 
     #[input]
-    amplitude: f32,
+    amount: f32,
 
     #[output]
     sample: f32,
 
-    carrier: rume::OwnedLut,
-    // modulator: rume::OwnedLut,
-    sample_period: f32,
+    phase: [f32; 2],
+    inv_sample_rate: f32,
 }
 
-impl Oscillator {
-    pub fn new() -> Self {
-        let mut osc = Self::default();
-        osc.carrier = rume::OwnedLut::new(
-            |x: f32| libm::sin(x as f64 * 2. * core::f64::consts::PI) as f32,
-            table::SIZE,
-        );
-        // osc.modulator = osc.carrier.clone();
-        osc
-    }
-}
-
-impl rume::Processor for Oscillator {
-    fn prepare(&mut self, config: rume::AudioConfig) {
-        self.sample_period = 1.0 / config.sample_rate as f32;
+impl Processor for Sine {
+    fn prepare(&mut self, data: AudioConfig) {
+        self.inv_sample_rate = 1.0 / data.sample_rate as f32;
     }
 
     fn process(&mut self) {
-        self.carrier.phasor.inc(self.frequency * table::TIME);
-        self.sample = self.carrier.advance();
+        const TWO_PI: f32 = 2.0_f32 * core::f32::consts::PI;
 
-        /*
-        self.carrier
-            .phasor
-            .inc(self.frequency * sample * self.fm * table::TIME);
+        let increment = TWO_PI * self.frequency * self.inv_sample_rate;
+        self.phase[0] = (self.phase[0] + increment) % TWO_PI;
+        self.sample = self.phase[0].sin();
 
-        self.sample = (self.carrier.advance() + sample) * self.amplitude * 0.5;
-        // self.sample = sample * 0.1;\
-        */
+        let increment = TWO_PI * self.frequency * self.inv_sample_rate * self.sample * self.amount;
+        self.phase[1] = (self.phase[1] + increment) % TWO_PI;
+        self.sample += self.phase[1].sin();
+
+        self.sample *= self.amplitude;
     }
 }
 
-rume::graph! {
+#[derive(Debug, Clone)]
+enum EnvelopeState {
+    Off = 0,
+    Attack,
+    Decay,
+    Sustain,
+    Release,
+}
+
+impl Default for EnvelopeState {
+    fn default() -> EnvelopeState {
+        EnvelopeState::Off
+    }
+}
+
+#[rume::processor]
+pub struct Envelope {
+    #[output]
+    amplitude: f32,
+
+    sample_rate: f32,
+    state: EnvelopeState,
+
+    #[input]
+    attack_delta: f32,
+    #[input]
+    decay_delta: f32,
+    #[input]
+    sustain_level: f32,
+    #[input]
+    release_delta: f32,
+
+    #[input]
+    note_on: f32,
+    #[input]
+    note_off: f32,
+}
+
+impl Processor for Envelope {
+    fn prepare(&mut self, data: rume::AudioConfig) {
+        self.sample_rate = data.sample_rate as f32;
+    }
+
+    fn process(&mut self) {
+        if self.note_on >= 1.0 {
+            self.state = EnvelopeState::Attack;
+            self.note_on = 0.0;
+        }
+
+        if self.note_off >= 1.0 {
+            self.state = EnvelopeState::Release;
+            self.note_off = 0.0;
+        }
+
+        match self.state {
+            EnvelopeState::Attack => {
+                self.amplitude += self.attack_delta;
+                if self.amplitude >= 1.0 {
+                    self.amplitude = 1.0;
+                    self.state = EnvelopeState::Decay;
+                }
+            }
+            EnvelopeState::Decay => {
+                self.amplitude -= self.decay_delta;
+                if self.amplitude <= self.sustain_level {
+                    if self.amplitude <= 0.0 {
+                        self.state = EnvelopeState::Off;
+                    } else {
+                        self.amplitude = self.sustain_level;
+                        self.state = EnvelopeState::Sustain;
+                    }
+                }
+            }
+            EnvelopeState::Sustain => {
+                self.amplitude = self.sustain_level;
+            }
+            EnvelopeState::Release => {
+                self.amplitude -= self.release_delta;
+                if self.amplitude <= 0.0 {
+                    self.amplitude = 0.0;
+                    self.state = EnvelopeState::Off;
+                }
+            }
+            EnvelopeState::Off => {
+                self.amplitude = 0.0;
+            }
+        }
+    }
+}
+graph! {
     inputs: {
+        note_on: { kind: trigger },
+        note_off: { kind: trigger },
         freq: { init: 220.0, range: 16.0..880.0, smooth: 10 },
-        fm:   { init:   6.0, range:  0.1..16.0 },
-        amp:  { init:   0.1, range:  0.0..0.8,   smooth: 10 },
+        fm_amt: { init: 2.0, range: 0.01..16.0, smooth: 10 },
+        dist_amt: { init: 0.2, range: 0.01..0.99, smooth: 10 },
+        attack: { init: 1.0, range: 0.01..10.0, smooth: 10 },
+        decay: { init: 2.0, range: 0.01..10.0, smooth: 10 },
+        sustain: { init: 2.0, range: 0.01..10.0, smooth: 10 },
+        release: { init: 2.0, range: 0.01..10.0, smooth: 10 },
+        val: { init: 125.0, range: 10.0..2000.0, smooth: 10 },
     },
     outputs: {
-        out,
+        audio_out,
     },
     processors: {
-        osc: Oscillator::new(),
+        sine: Sine::default(),
+        dist: Distortion::default(),
+        env: Envelope::default(),
+        dly: Delay::default(),
     },
     connections: {
-        freq.output ->  osc.input.0,
-        fm.output   ->  osc.input.1,
-        amp.output  ->  osc.input.2,
-        osc.output  ->  out.input,
+        freq.output    ->  sine.input.0,
+        env.output     ->  sine.input.1,
+        fm_amt.output  ->  sine.input.2,
+
+        attack.output   ->  env.input.0,
+        decay.output    ->  env.input.1,
+        sustain.output  ->  env.input.2,
+        release.output  ->  env.input.3,
+
+        note_on.output  ->  env.input.4,
+        note_off.output ->  env.input.5,
+
+        sine.output     ->  dist.input.0,
+        dist_amt.output ->  dist.input.1,
+        dist.output     ->  dly.input.0,
+        val.output      ->  dly.input.1,
+        dly.output      ->  audio_out.input,
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -37,9 +37,37 @@ fn main() -> ! {
     let mut audio_interface = board.SAI1;
     let mut midi_interface = board.USART1;
 
+    let (mut graph, mut inputs, mut outputs) = dsp::build();
+
+    // - usart1 interrupt -----------------------------------------------------
+
+    let mut midi_parser = midi::Parser::new();
+
+    // let (mut freq, mut fm, mut amp) = (inputs.freq, inputs.fm, inputs.amp);
+
+    midi_interface
+        .start(|byte| {
+            led_user.on();
+            // if was_high {
+            //     led_user.off();
+            // } else {
+            // }
+            // midi_parser.rx(byte, |_channel, message| match message {
+            //     Message::NoteOn { note, velocity } => {
+            //         let _ = freq.enqueue(convert::pitch::from_midi(note));
+            //         led_user.on();
+            //     }
+            //     Message::NoteOff { note, velocity } => {
+            //         led_user.off();
+            //     }
+            //     Message::ControlChange { index, value } => {}
+            //     Message::ProgramChange { value } => {}
+            // });
+        })
+        .unwrap();
+
     // - audio callback -------------------------------------------------------
 
-    let (mut graph, mut inputs, mut outputs) = dsp::build();
     graph.prepare(rume::AudioConfig {
         sample_rate: daisy::audio::FS.0 as usize,
         buffer_size: daisy::audio::BLOCK_LENGTH,
@@ -49,43 +77,15 @@ fn main() -> ! {
     audio_interface.start(move |_fs, block| {
         graph.process();
         for frame in block {
-            let sample = outputs.out.dequeue().unwrap();
+            let sample = outputs.audio_out.dequeue().unwrap();
             *frame = (sample, sample);
         }
     });
 
-    // - usart1 interrupt -----------------------------------------------------
-
-    let mut midi_parser = midi::Parser::new();
-
-    let (mut freq, mut fm, mut amp) = (inputs.freq, inputs.fm, inputs.amp);
-
-    // midi_interface
-    //     .start(|byte| {
-    //         midi_parser.rx(byte, |_channel, message| {
-    //             match message {
-    //                 Message::NoteOn { note, velocity } => {
-    //                     let _ = freq.enqueue(convert::pitch::from_midi(note));
-    //                     led_user.on();
-    //                 }
-    //                 Message::NoteOff { note, velocity } => {
-    //                     led_user.off();
-    //                 }
-    //                 Message::ControlChange { index, value } => {}
-    //                 Message::ProgramChange { value } => {}
-    //             }
-    //         });
-    //     })
-    //     .unwrap();
-
-    // - main loop ------------------------------------------------------------
-
-    // - clocks ---------------------------------------------------------------
+    // - adc ------------------------------------------------------------------
 
     // switch adc_ker_ck_input multiplexer to per_ck
     board.peripheral.kernel_adc_clk_mux(AdcClkSel::PER);
-
-    // - adc ------------------------------------------------------------------
 
     let cp = unsafe { cortex_m::Peripherals::steal() };
     let dp = unsafe { pac::Peripherals::steal() };
@@ -96,25 +96,25 @@ fn main() -> ! {
     adc1.set_resolution(adc::Resolution::SIXTEENBIT);
 
     let gpioc = dp.GPIOC.split(board.peripheral.GPIOC);
-
     let mut adc1_channel_4 = gpioc.pc4.into_analog(); // pot 1
     let mut adc1_channel_10 = gpioc.pc0.into_analog(); // pot 2
 
     // - main loop ------------------------------------------------------------
 
+    let _ = inputs.note_on.enqueue(1.0);
     loop {
         let val = {
             let val: u32 = adc1.read(&mut adc1_channel_10).unwrap();
-            (val as f32 * (880. / 65_535.))
+            880. - (val as f32 * (880. / 65_535.))
         };
-        let _ = freq.enqueue(val);
+        let _ = inputs.freq.enqueue(val);
 
-        let val = {
-            let val: u32 = adc1.read(&mut adc1_channel_4).unwrap();
-            (val as f32 * (0.9 / 65_535.))
-        };
-        let _ = amp.enqueue(val);
+        // let val = {
+        //     let val: u32 = adc1.read(&mut adc1_channel_4).unwrap();
+        //     (val as f32 * (0.9 / 65_535.))
+        // };
+        // let _ = amp.enqueue(val);
 
-        asm::delay(50_000);
+        cortex_m::asm::wfi();
     }
 }
