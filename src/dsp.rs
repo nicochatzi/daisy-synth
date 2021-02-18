99724d54e796f089f3d91e7d710f7000ff1bee21
Nico Chatzi <nico.chatzigianis@focusrite.com>
2021-02-18 10:19:30 +0000

    Initial commit

---
+++ src/dsp.rs
@@ -0,0 +1,77 @@
+use alloc::boxed::Box;
+use rume::*;
+
+mod table {
+    pub const SIZE: usize = 256;
+    pub const FREQ: f32 = 48_000. / SIZE as f32;
+    pub const TIME: f32 = 1. / FREQ;
+}
+
+#[rume::processor]
+pub struct Oscillator {
+    #[input]
+    frequency: f32,
+
+    #[input]
+    fm: f32,
+
+    #[input]
+    amplitude: f32,
+
+    #[output]
+    sample: f32,
+
+    carrier: rume::OwnedLut,
+    modulator: rume::OwnedLut,
+    sample_period: f32,
+}
+
+impl Oscillator {
+    pub fn new() -> Self {
+        let mut osc = Self::default();
+        osc.carrier = rume::OwnedLut::new(
+            |x: f32| libm::sin(x as f64 * 2. * core::f64::consts::PI) as f32,
+            table::SIZE,
+        );
+        osc.modulator = osc.carrier.clone();
+        osc
+    }
+}
+
+impl rume::Processor for Oscillator {
+    fn prepare(&mut self, config: rume::AudioConfig) {
+        self.sample_period = 1.0 / config.sample_rate as f32;
+    }
+
+    fn process(&mut self) {
+        self.modulator.phasor.inc(self.frequency * table::TIME);
+        let sample = self.modulator.advance();
+
+        self.carrier
+            .phasor
+            .inc(self.frequency * sample * self.fm * table::TIME);
+
+        self.sample = (self.carrier.advance() + sample) * self.amplitude * 0.5;
+        // self.sample = sample * 0.1;
+    }
+}
+
+rume::graph! {
+    inputs: {
+        freq: { init: 220.0, range: 16.0..880.0, smooth: 10 },
+        fm:   { init:   6.0, range:  0.1..16.0 },
+        amp:  { init:   0.1, range:  0.0..0.8,   smooth: 10 },
+    },
+    outputs: {
+        out,
+    },
+    processors: {
+        osc: Oscillator::new(),
+    },
+    connections: {
+        freq.output ->  osc.input.0,
+        fm.output   ->  osc.input.1,
+        amp.output  ->  osc.input.2,
+        osc.output  ->  out.input,
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,96 @@
+#![no_main]
+#![no_std]
+#![feature(alloc_error_handler)]
+
+extern crate alloc;
+
+use cortex_m::asm;
+use cortex_m_rt::entry;
+use panic_semihosting as _;
+
+use daisy::led::Led;
+use daisy_bsp as daisy;
+
+use daisy::hal::prelude::*;
+
+use rume::*;
+mod dsp;
+mod midi;
+use midi::Message;
+
+#[entry]
+fn main() -> ! {
+    // - board setup ----------------------------------------------------------
+
+    let mut board = daisy::Board::take().unwrap();
+
+    let mut led_user = board.leds.USER;
+    let mut audio_interface = board.SAI1;
+    let mut midi_interface = board.USART1;
+
+    // - audio callback -------------------------------------------------------
+
+    let (mut graph, mut inputs, mut outputs) = dsp::build();
+    graph.prepare(rume::AudioConfig {
+        sample_rate: daisy::audio::FS.0 as usize,
+        buffer_size: daisy::audio::BLOCK_LENGTH,
+        num_channels: 2,
+    });
+
+    let _audio_interface = audio_interface.start(move |_fs, block| {
+        // graph.process();
+        // for frame in block {
+        //     let sample = outputs.out.dequeue().unwrap();
+        //     *frame = (sample, sample);
+        // }
+    });
+
+    // - usart1 interrupt -----------------------------------------------------
+
+    let mut midi_parser = midi::Parser::new();
+
+    let (mut freq, mut fm, mut amp) = (inputs.freq, inputs.fm, inputs.amp);
+
+    midi_interface
+        .start(|byte| {
+            midi_parser.rx(byte, |_channel, message| match message {
+                Message::NoteOn { note, velocity } => {
+                    let _ = freq.enqueue(convert::pitch::from_midi(note));
+                }
+                Message::NoteOff { note, velocity } => {}
+                Message::ControlChange { index, value } => {}
+                Message::ProgramChange { value } => {}
+            });
+        })
+        .unwrap();
+
+    // - main loop ------------------------------------------------------------
+
+    // let mut knobs = (
+    //     board.pins.SEED_15.into_analog(),
+    //     board.pins.SEED_16.into_analog(),
+    //     board.pins.SEED_17.into_analog(),
+    //     board.pins.SEED_18.into_analog(),
+    // );
+
+    let one_second = board.clocks.sys_ck().0;
+
+    loop {
+        // if let Ok(data) = board.adc1.read(&mut knobs.0) {
+        //     let data: u32 = data;
+        //     let val = (1. + (data as f32 / board.adc1.max_sample() as f32)) * 64.;
+        //     if let Ok(_) = freq.enqueue(val) {}
+        // }
+        // if let Ok(data) = board.adc1.read(&mut knobs.1) {
+        //     let data: u32 = data;
+        //     let val = (1. + (data as f32 / board.adc1.max_sample() as f32)) * 8.;
+        //     let _ = fm.enqueue(8.0);
+        // }
+        // if let Ok(data) = board.adc1.read(&mut knobs.1) {
+        //     let data: u32 = data;
+        //     let freq = ((data as f32 / board.adc1.max_sample() as f32) + 1.0) * 64.0;
+        //     if let Ok(_) = inputs.amp.enqueue(freq) {}
+        // }
+        asm::delay(10_000);
+    }
+}
